bb7f71e1633cec927c1184f398479467eac33faf
furiousme <shuptaroks@gmail.com>
2024-11-26 18:36:58 +0200

    feat(fe:auth): create signup page

---
+++ apps/web/app/auth/signup/page.tsx
@@ -0,0 +1,45 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+import SignUpForm from "./signup-form";
+
+const SignUpPage = () => {
+  return (
+    <div className="container relative min-h-screen flex items-center justify-center lg:max-w-none px-3">
+      <Link
+        href="/examples/authentication"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Login
+      </Link>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <SignUpForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
